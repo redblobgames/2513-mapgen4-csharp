18438c535f38a2b0ec8a9d12e2f445608a6a6f03
Amit Patel <redblobgames@gmail.com>
2025-03-29 11:21:57 -0700

    Feat: draw a dual-mesh test diagram to the screen
    The dual-mesh is generated from C# (compiled to wasm) and drawn to the
    screen with JS.
    
    Use "dotnet run" to run this.

--- Program.cs
+++ Program.cs
@@ -13,21 +13,36 @@ partial class Mapgen4
 {
     private static Delaunator _delaunator;
 
-    [JSImport("dom.setInnerText", "main.js")]
-    internal static partial void SetInnerText(string selector, string content);
+    [JSImport("canvas.drawPoints", "main.js")]
+    internal static partial void DrawPoints(
+        string color, double radius,
+        [JSMarshalAs<JSType.Array<JSType.Number>>] double[] coordinates
+        );
+
+    [JSImport("canvas.drawLineSegments", "main.js")]
+    internal static partial void DrawLineSegments(
+        string color, double lineWidth,
+        [JSMarshalAs<JSType.Array<JSType.Number>>] double[] coordinates
+        );
 
     [JSExport]
     internal static void RunDualMesh()
     {
-        var bounds = new Bounds { Left = 0, Top = 0, Width = 1000, Height = 1000 };
+        var bounds = new Bounds { Left = 50, Top = 50, Width = 900, Height = 900 };
         var spacing = 50;
         var points = MeshCreator.GenerateInteriorBoundaryPoints(bounds, spacing);
         var numBoundaryPoints = points.Count;
 
-        for (int q = 0; q < bounds.Width / spacing; q++) {
-            for (int r = 0; r < bounds.Height / spacing; r++) {
-                double x = bounds.Left + q * spacing;
-                double y = bounds.Top + r * spacing;
+        // DualMeshTests.AddRandomPoints(points, (int)Math.Round((bounds.Width * bounds.Height) / (spacing * spacing)), bounds);
+
+        // Create a jittered grid as a cheaper alternative to a poisson disc set
+        var random = new Random();
+        for (int q = 1; q < bounds.Width / spacing; q++)
+        {
+            for (int r = 1; r < bounds.Height / spacing; r++)
+            {
+                double x = bounds.Left + q * spacing + (random.NextDouble()-random.NextDouble()) * spacing/3;
+                double y = bounds.Top + r * spacing + (random.NextDouble()-random.NextDouble()) * spacing/3;
                 points.Add(new Point(x, y));
             }
         }
@@ -36,5 +51,44 @@ partial class Mapgen4
         var init = new MeshInitializer{ Points = points, Triangles = _delaunator.Triangles, Halfedges = _delaunator.Halfedges, NumBoundaryPoints = numBoundaryPoints };
         init = TriangleMesh.AddGhostStructure(init);
         var mesh = new TriangleMesh(init);
+
+        var coordinates = new double[4 * mesh.NumSolidSides];
+        for (int s = 0; s < mesh.NumSolidSides; s++)
+        {
+            int r1 = mesh.R_Begin_S(s);
+            int r2 = mesh.R_End_S(s);
+            coordinates[4*s] = mesh.X_Of_R(r1);
+            coordinates[4*s+1] = mesh.Y_Of_R(r1);
+            coordinates[4*s+2] = mesh.X_Of_R(r2);
+            coordinates[4*s+3] = mesh.Y_Of_R(r2);
+        }
+        DrawLineSegments("black", 0.75, coordinates);
+
+        for (int s = 0; s < mesh.NumSolidSides; s++)
+        {
+            int t1 = mesh.T_Inner_S(s);
+            int t2 = mesh.T_Outer_S(s);
+            coordinates[4*s] = mesh.X_Of_T(t1);
+            coordinates[4*s+1] = mesh.Y_Of_T(t1);
+            coordinates[4*s+2] = mesh.X_Of_T(t2);
+            coordinates[4*s+3] = mesh.Y_Of_T(t2);
+        }
+        DrawLineSegments("white", 1.5, coordinates);
+
+        coordinates = new double[2 * mesh.NumSolidTriangles];
+        for (int t = 0; t < mesh.NumSolidTriangles; t++)
+        {
+            coordinates[2*t] = mesh.X_Of_T(t);
+            coordinates[2*t+1] = mesh.Y_Of_T(t);
+        }
+        DrawPoints("hsl(240 50% 50%)", 3, coordinates);
+        
+        coordinates = new double[2 * mesh.NumSolidRegions];
+        for (int r = 0; r < mesh.NumSolidRegions; r++)
+        {
+            coordinates[2*r] = mesh.X_Of_R(r);
+            coordinates[2*r+1] = mesh.Y_Of_R(r);
+        }
+        DrawPoints("hsl(0 50% 50%)", 4, coordinates);
     }
 }
--- wwwroot/main.js
+++ wwwroot/main.js
@@ -3,12 +3,44 @@ import { dotnet } from "./_framework/dotnet.js";
 const { setModuleImports, getAssemblyExports, getConfig } = await dotnet.withApplicationArguments("start").create();
 
 setModuleImports('main.js', {
-    dom: {
-        setInnerText: (selector, time) => document.querySelector(selector).innerText = time
-    }
+    canvas: {
+        drawPoints,
+        drawLineSegments,
+    },
 });
+
+/** @type{HTMLCanvasElement} */
+const BG_COLOR = "#cccccc";
+const canvas = document.querySelector("#diagram-output");
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = BG_COLOR;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+/** @type{number[]} coordinates - flattened x,y point coordinates */
+function drawPoints(color, radius, coordinates) {
+    ctx.fillStyle = color;
+    ctx.strokeStyle = BG_COLOR;
+    ctx.lineWidth = 2;
+    for (let i = 0; i < coordinates.length; i += 2) {
+        ctx.beginPath();
+        ctx.arc(coordinates[i], coordinates[i+1], radius, 0, 2*Math.PI);
+        ctx.stroke();
+        ctx.fill();
+    }
+}
+
+/** @type{number[]} coordinates - flattened x1,y1,x2,y2 point coordinates */
+function drawLineSegments(color, lineWidth, coordinates) {
+    ctx.strokeStyle = color;
+    ctx.lineWidth = lineWidth;
+    for (let i = 0; i < coordinates.length; i += 4) {
+        ctx.beginPath();
+        ctx.moveTo(coordinates[i], coordinates[i+1]);
+        ctx.lineTo(coordinates[i+2], coordinates[i+3]);
+        ctx.stroke();
+    }
+}
 
 const config = getConfig();
 const exports = await getAssemblyExports(config.mainAssemblyName);
-
 console.log(exports.Mapgen4.RunDualMesh());
